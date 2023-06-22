5ef26f24302cd0470e34b634f4cec9d426b3bb68
johnathanhawn <43219447+johnathanhawn@users.noreply.github.com>
2023-06-21 22:07:23 -0700

    Update idc.lua

--- idc.lua
+++ idc.lua
@@ -1,4 +1,5 @@
 local csFilePath = "cs.txt"
+local textutilsAvailable = textutils and textutils.serialize ~= nil
 
 -- Check if the cs.txt file exists
 if not fs.exists(csFilePath) then
@@ -34,42 +35,11 @@ local function connectToWebSocket()
         -- Process the received command
         local success, result = pcall(loadstring(message))
 
-        -- Check if the command is a movement command
-        local isMovementCommand = false
-        if message == "turtle.forward()" or message == "turtle.back()" or message == "turtle.left()" or message == "turtle.right()" or message == "turtle.up()" or message == "turtle.down()" then
-          isMovementCommand = true
-        end
+        -- Serialize the result to send as a string
+        local serializedResult = textutilsAvailable and textutils.serialize(result) or tostring(result)
 
-        -- If it's a movement command, inspect surroundings
-        if isMovementCommand then
-          local inspectResult = turtle.inspect()
-          if inspectResult then
-            ws.send("Inspect result: " .. inspectResult.name)
-          else
-            ws.send("Unable to inspect surroundings")
-          end
-        end
-
-        -- Check if the command execution was successful
-        if success then
-          -- Check if the command failed
-          if not turtle.success() then
-            ws.send("Command execution failed")
-          else
-            -- Check if the result is a table
-            if type(result) == "table" then
-              -- Convert the table to a string representation
-              local serializedResult = textutils.serialize(result)
-              ws.send("Command executed successfully:\n" .. serializedResult)
-            else
-              -- Return the result as a string
-              ws.send("Command executed successfully: " .. tostring(result))
-            end
-          end
-        else
-          -- Return the error message if command execution failed
-          ws.send("Command execution error: " .. tostring(result))
-        end
+        -- Return the serialized result as a string
+        ws.send(serializedResult)
       end
     end
 
