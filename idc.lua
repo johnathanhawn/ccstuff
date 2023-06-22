9f96fb9355a974e7829cdd3e793dcf4bd493f876
johnathanhawn <43219447+johnathanhawn@users.noreply.github.com>
2023-06-21 20:08:55 -0700

    Update idc.lua

--- idc.lua
+++ idc.lua
@@ -1,14 +1,10 @@
 local ws, err = http.websocket("ws://your-websocket-server-url")
-
 if ws then
   print("WebSocket server connected")
-
   -- Send a welcome message to the client
   ws.send("Turtle Here")
-
   while true do
     local message = ws.receive()
-
     if message then
       -- Process the received command
       local response = processCommand(message)
@@ -17,17 +13,14 @@ if ws then
       ws.send(response)
     end
   end
-
   ws.close()
   print("WebSocket server disconnected")
 else
   print("Failed to connect to the WebSocket server: " .. err)
 end
-
 function processCommand(command)
   -- Run the command on the turtle shell
   shell.run(command)
-
   -- Return an acknowledgment message
   return "Command executed: " .. command
 end
